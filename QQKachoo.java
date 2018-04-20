175a20238ef6f37b005cf0ea73b4423028ce662f
Aidan Griffin <agriffin@stuy.edu>
2018-04-20 14:03:52 -0400

    important changes to stuff

--- QQKachoo.java
+++ QQKachoo.java
@@ -1,31 +1,37 @@
-import java.util.ArrayList;
+
 
 public class QQKachoo<Card> implements deque<Card>{
 
-    private ArrayList<Card> _Deque;
-
+    private DLLNode<Card> _head;
+    private DLLNode<Card> _tail;
+    private int _size;
     public QQKachoo(){
-	_Deque = new ArrayList<Card>();
+	_Deque = new DLLNode<Card>();
+	_size = 0;
     }
 
-public int size() {
-return _Deque.size();
-}
+    public int size() {
+	return _size;
+    }
 
     public void addFirst(Card x){
-	_Deque.add(0,x);
+	_head = new DLLNode<Card>(x, null, _head);
     }
 
     public void addLast(Card x){
-	_Deque.add(x);
+	_tail = new DLLNode<Card>(x, null, _tail);
     }
 
     public Card removeFirst(){
-	return _Deque.remove(0);
+	Card temp = _head.getCargo();
+	_head = _head.getNext();
+	return temp;
     }
 
     public Card removeLast(){
-	return _Deque.remove(_Deque.size() - 1);
+	Card temp = _tail.getCargo();
+	_tail = _tail.getNext();
+	return temp;
     }
 
     public Card getFirst(){
