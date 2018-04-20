b68be2c63ef1fee0315365288b923c49bd934ed7
sroudnitsky <33501501+sroudnitsky@users.noreply.github.com>
2018-04-19 22:44:17 -0400

    Added QQKachoo.java

---
+++ QQKachoo.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class QQKachoo<Card> implements Deque<Card>{
+
+    private ArrayList<Card> _Deque;
+
+    public QQKachoo(){
+	_Deque = new ArrayList<Card>();
+    }
+
+    public void addFirst(Card x){
+	_Deque.add(0,x);
+    }
+
+    public void addLast(Card x){
+	_Deque.add(x);
+    }
+
+    public Card removeFirst(){
+	return _Deque.remove(0);
+    }
+
+    public Card removeLast(){
+	return _Deque.remove(_Deque.size() - 1);
+    }
+
+    public Card getFirst(){
+	return _Deque.get(0);
+    }
+
+    public Card getLast(){
+	return _Deque.get(_Deque.size() - 1);
+    }
