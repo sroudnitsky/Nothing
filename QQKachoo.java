522f751f7488a6788eb372c9053578466919b850
YingXin7 <yjiang2@stuy.edu>
2018-04-25 20:42:30 -0400

    added comments but didn't change the coding

--- QQKachoo.java
+++ QQKachoo.java
@@ -1,42 +1,48 @@
-
-import java.util.ArrayList;
-
 public class QQKachoo<Card> implements deque<Card>{
     
-    private ArrayList<Card> _Deque;
+    private DLLNode<Card> _Deque;
     
+//constructor
     public QQKachoo(){
-	_Deque = new ArrayList<Card>();
+	_Deque = new DLLNode<Card>();
     }
     
+//adds new element at head
     public void addFirst(Card x){
 	_Deque.add(0,x);
     }//O(n)
     
+//adds new element at tail
     public void addLast(Card x){
 	_Deque.add(x);
     }//O(1)
     
+//remove 1st element
     public Card removeFirst(){
 	return _Deque.remove(0);
     }//O(n)
     
+//remove last element
     public Card removeLast(){
 	return _Deque.remove(_Deque.size() - 1);
     }//O(1)
     
+//return 1st element
     public Card getFirst(){
 	return _Deque.get(0);
     }//O(1)
     
+//return last element
 public Card getLast(){
 	return _Deque.get(_Deque.size() - 1);
     }//O(1)
 
+//return size of linked list
     public int size(){
 	return _Deque.size();
     }//O(1)
 
+//check whether the list has a certain element
     public boolean contains(Card e){
 	for (int i = 0; i < _Deque.size(); i++){
 	    if (_Deque.get(i).equals(e)){
@@ -46,6 +52,7 @@ public Card getLast(){
 	return false;
     }//O(n)
 
+//remove 1st occurance of a certain element
     public boolean remove(Card e){
 	int counter = 0;
 	for (int i = 0; i < _Deque.size(); i++){
